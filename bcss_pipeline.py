4908483be265179628f44b8d4386e0f6753bf561
arina <adromashkina_1@edu.hse.ru>
2026-02-09 16:48:55 +0300

    mnist pipeline fix

--- bcss_pipeline.py
+++ bcss_pipeline.py
@@ -27,19 +27,21 @@ data = torch.load(PATH_DATA)
 train_ds = create_score_feature_dataset_bcss(data, DEVICE)
 
 print("Training separate flows for each class...")
-separate_flows = SeparateClassFlows(num_classes=NUM_CLASSES, n_flows=10, feature_dim=64, hidden_dim=64).to(DEVICE)
-# separate_flows = separate_flows.train_separate(train_ds, epochs=3, lr=1e-3, device=DEVICE)
-# torch.save(separate_flows.state_dict(), 'BCSS/bcss_cond_flows_model.pth')
+separate_flows = SeparateClassFlows(num_classes=NUM_CLASSES, n_flows=8, feature_dim=64, hidden_dim=64).to(DEVICE)
+separate_flows = separate_flows.train_separate(train_ds, epochs=5, lr=1e-3, device=DEVICE)
+torch.save(separate_flows.state_dict(), 'BCSS/bcss_cond_flows_model.pth')
 
-test_file_name = '../../../../blob/BCSS/TCGA-S3-AA10-DX1_xmin43039_ymin23986_MPP-0.2500.png.tensor'
+test_file_name = '../../../../blob/BCSS/test/TCGA-S3-AA10-DX1_xmin43039_ymin23986_MPP-0.2500.png.tensor'
 test_data = torch.load(test_file_name, weights_only=False)
 features = test_data['features']
 predictions = test_data['predictions']
 labels = torch.tensor(test_data['mask'])
-
-predictions = torch.flatten(predictions, start_dim=2)    
-features = torch.flatten(features, start_dim=2)    
+predictions = torch.flatten(predictions, start_dim=2).squeeze(0).T    
+features = torch.flatten(features, start_dim=2).squeeze(0).T    
 labels = torch.flatten(labels, start_dim=0)
-print(features.shape)
-print(predictions.shape)
-print(labels.shape)
+test_ds = ScoreFeatureDataset(predictions, features, predictions, labels)
+
+scores_flow, decoys_flow, labels_flow = separate_flows.generate_decoys(test_ds, device='cuda')
+torch.save(scores_flow, 'scores_flow.pt')
+torch.save(decoys_flow, 'decoys_flow.pt')
+torch.save(labels_flow, 'labels_flow.pt')
